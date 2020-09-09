eb0c2790fb1a0068a381e2ce3bbc260b39e6fd11
Asher Williams <asher.williams1998@gmail.com>
2020-09-09 10:37:22 -0500

    created initial project, along with test project, and data access project

---
+++ Police-RDBMS/Police-RDBMS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Police_RDBMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
